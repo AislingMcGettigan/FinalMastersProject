eb0ad0e79c8f209b835da6cdfc06151cc5a11767
AislingMcGettigan <aislingdeveloper@hotmail.com>
2018-10-24 22:28:56 +0100

    filters for type and postcode added

--- FinalMastersProject/ProviderDetails.aspx.cs
+++ FinalMastersProject/ProviderDetails.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using Microsoft.AspNet.Identity;
 
 namespace FinalMastersProject
 {
@@ -12,6 +14,98 @@ namespace FinalMastersProject
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btnSubmitReviewDetails_Click(object sender, EventArgs e)
+        {
+            string Connstr = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+            SqlConnection conn = new SqlConnection(Connstr);
+            conn.Open();
+
+            string addReview = "Insert into Review (providerId, userId, reviewTitle, reviewBody, addedDate) values(@providerId, @userid, @reviewTitle, @reviewBody, @addedDate)";
+            SqlCommand sqlcmdComment = new SqlCommand(addReview, conn);
+            sqlcmdComment.Parameters.AddWithValue("@providerId", Convert.ToInt16(Request.QueryString["ProviderId"]));
+            sqlcmdComment.Parameters.AddWithValue("@userid", User.Identity.GetUserId());
+            sqlcmdComment.Parameters.AddWithValue("@reviewTitle", TbReviewTitle.Text);
+            sqlcmdComment.Parameters.AddWithValue("@reviewBody", TbReviewBody.Text);
+            sqlcmdComment.Parameters.AddWithValue("@AddedDate", DateTime.Now);
+            sqlcmdComment.ExecuteNonQuery();
+
+
+            for (int ct = 0; ct < CBLExistingTags.Items.Count; ct++)
+            {
+                if (CBLExistingTags.Items[ct].Selected) Response.Write("-" + CBLExistingTags.Items[ct].Value);
+
+                if (CBLExistingTags.Items[ct].Selected) ConnectTagAndProduct(Convert.ToInt16(CBLExistingTags.Items[ct].Value), Convert.ToInt16(Request.QueryString["ProductId"]), conn);
+            }
+
+
+            Response.Redirect("ProviderDetails.aspx?ProviderId=" + Request.QueryString["ProviderId"]);
+        }
+
+        protected void btnAddNewTags_Click(object sender, EventArgs e)
+        {
+            string Connstr = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            SqlConnection conn = new SqlConnection(Connstr);
+            conn.Open();
+
+            if (TbTags.Text == "") return;
+
+            string[] newTags = TbTags.Text.Split(',');
+
+            foreach (string Tag in newTags)
+            {
+                string addTagSql = "INSERT INTO Tag(tag) VALUES (@Tag)";
+                SqlCommand sqlcmd = new SqlCommand(addTagSql, conn);
+                sqlcmd.Parameters.AddWithValue("@Tag", Tag);
+                sqlcmd.ExecuteNonQuery();
+            }
+
+            Response.Redirect("ProviderDetails.aspx?ProviderId=" + Request.QueryString["ProviderId"]);
+
+        }
+
+
+        protected int AddTagToDB(string Tag, SqlConnection conn)
+        {
+            string addTagSql = "INSERT INTO Tag(tag) VALUE (@Tag)";
+            SqlCommand sqlcmd = new SqlCommand(addTagSql, conn);
+            sqlcmd.Parameters.AddWithValue("@Tag", Tag);
+            sqlcmd.ExecuteNonQuery();
+            string lastTagAddedSql = "SELECT TOP 1 Id FROM Tag ORDER BY Id DESC";
+            SqlCommand sqlcmd2 = new SqlCommand(lastTagAddedSql, conn);
+            SqlDataReader sqlDr = sqlcmd.ExecuteReader();
+            sqlDr.Read();
+            int retVal = Convert.ToInt16(sqlDr["Id"]);
+            sqlDr.Close();
+            return retVal;
+
+        }
+
+        protected void ConnectTagAndProduct(int TagId, int ProviderId, SqlConnection conn)
+        {
+            string addTagAndProvider = "INSERT INTO TagJunction(tagId,providerId) VALUES (@TagId,@ProviderId)";
+            SqlCommand sqlcmd = new SqlCommand(addTagAndProvider, conn);
+            sqlcmd.Parameters.AddWithValue("@TagId", TagId);
+            sqlcmd.Parameters.AddWithValue("@ProviderId", ProviderId);
+            sqlcmd.ExecuteNonQuery();
+        }
+
+        protected int GetIdForTag(string Tag, SqlConnection conn)
+        {
+            int retVal = -1;
+            string findTagSql = "SELECT Id FROM Tag WHERE tag=@Tag";
+            SqlCommand sqlCmd = new SqlCommand(findTagSql, conn);
+            sqlCmd.Parameters.AddWithValue("@Tag", Tag);
+            SqlDataReader sqlDr = sqlCmd.ExecuteReader();
+            if (sqlDr.HasRows)
+            {
+                sqlDr.Read();
+                retVal = Convert.ToInt16(sqlDr["Id"]);
+            }
+            sqlDr.Close();
+            return retVal;
         }
     }
 }
--- FinalMastersProject/Providers.aspx
+++ FinalMastersProject/Providers.aspx
@@ -19,13 +19,15 @@
             width: 100%;
         }
     </style>
-    <div>
-    <b>Filter Type:</b>
-            <asp:DropDownList ID="ddlType" runat="server" CssClass="form-control centerDD" Width="30%" AutoPostBack="True" DataSourceID="SqlDataSource1" DataTextField="type" DataValueField="type" AppendDataBoundItems="True">
-                <asp:ListItem Text="Select Full or Half Day Care " Value="" />
-            </asp:DropDownList>
+    <div class="center centerDD">
+        <b>Filter Type:</b>
+        <asp:DropDownList ID="ddlType" runat="server" CssClass="form-control centerDD" Width="30%" AutoPostBack="True" DataSourceID="SqlDataSource1" DataTextField="type" DataValueField="type" AppendDataBoundItems="True">
+            <asp:ListItem Text="Select Full or Half Day Care " Value="" />
+        </asp:DropDownList>
         <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:DefaultConnection %>" SelectCommand="SELECT [type] FROM [ProviderType]"></asp:SqlDataSource>
-        </div>
+
+        <asp:TextBox ID="tbPostCode" placeholder="Post Code Search" runat="server" AutoPostBack="True" CssClass="form-control centerDD"></asp:TextBox>
+    </div>
 
 
     <div class="center-div">
@@ -176,7 +178,12 @@
             </SelectedItemTemplate>
         </asp:ListView>
 
-        <asp:SqlDataSource ID="providerQuery" runat="server" ConnectionString="<%$ ConnectionStrings:DefaultConnection %>" SelectCommand="SELECT prov.[id], proImg.[imagePath], prov.[name], proTy.[type], proAdd.[postcode], prov.[priceByDay] FROM [Provider] prov INNER JOIN [ProviderType] proTy ON proTy.Id = prov.providerTypeId INNER JOIN [ProviderAddress] proAdd ON proAdd.Id = prov.providerAddressId INNER JOIN [ProviderImages] proImg ON proImg.Id = prov.providerImageId"></asp:SqlDataSource>
+        <asp:SqlDataSource ID="providerQuery" runat="server" ConnectionString="<%$ ConnectionStrings:DefaultConnection %>" SelectCommand="SELECT prov.[id], proImg.[imagePath], prov.[name], proTy.[type], proAdd.[postcode], prov.[priceByDay] FROM [Provider] prov INNER JOIN [ProviderType] proTy ON proTy.Id = prov.providerTypeId INNER JOIN [ProviderAddress] proAdd ON proAdd.Id = prov.providerAddressId INNER JOIN [ProviderImages] proImg ON proImg.Id = prov.providerImageId WHERE proTy.[type] LIKE @providerType AND proAdd.[postcode] LIKE (@postCode + '%')">
+            <SelectParameters>
+                <asp:ControlParameter ControlID="ddlType" DefaultValue="%" Name="providerType" PropertyName="SelectedValue" Type="String" />
+                <asp:ControlParameter  ControlID="tbPostCode" DefaultValue="%" Name="postCode" PropertyName="Text" Type="String" />
+            </SelectParameters>
+        </asp:SqlDataSource>
     </div>
 </asp:Content>
     
